56fbe4e51e6339184eb6a95cdf2eccb0656730db
ecobu <65851040+ecobu@users.noreply.github.com>
2020-05-31 14:09:09 +0900

    Updates

---
+++ precache-manifest.d21d41c159544b37131b186758259b09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8070f8abe0e7f8a14c62830b76fe17d4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "411aec71257b9aa4e89c",
+    "url": "/movie_app/static/js/2.cb9a36d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cb9a36d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1c5affab6d369dde37",
+    "url": "/movie_app/static/js/main.088396f9.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
